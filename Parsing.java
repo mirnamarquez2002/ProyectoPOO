53600b17008ab0ba9beda2722b99f29d372706cc
Vlad GAG <vpikosh@gmail.com>
2023-06-13 13:02:31 -0600

    Changed Parsing.java

--- Parsing.java
+++ Parsing.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
+import polynomial_pkg.*;
 
 public class Parsing{
 
     private ArrayList<Integer> parsedArray;
+    private Polynomial polyArray = new Polynomial();
 
     //Constructor instancing parsedArray
     public Parsing(){
